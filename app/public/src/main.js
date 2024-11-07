c89d04d7ddc5c8c09de023dd2da20a4f6a400d14
Giordano de Brito <megagamesb@gmail.com>
2024-11-06 21:43:33 -0300

    jsDoc notations

--- app/public/src/main.js
+++ app/public/src/main.js
@@ -6,11 +6,24 @@ window.addEventListener('load', function ()
 	defineControls();
 });
 
+/**
+* Define the main controls of the header tools.
+* @returns {void}
+*/
 function defineControls ()
 {
 	let header = document.querySelector('header');
 
 	//let icons = Array.from(header.querySelectorAll('img'));
 
-	header.children[0].onclick = () => window.electronAPI.closeApp();
+	header.children[1].onclick = () => quit();
+}
+
+/**
+* Closes the current application instance.
+* @returns {void}
+*/
+function quit ()
+{
+	window.electronAPI.closeApp();
 }
